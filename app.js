6a033d0b14afbf9fc5a7ae47f40dca8e4dd9ee5c
muddassiryahya <m.m2442212@gmail.com>
2023-10-15 20:57:41 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,189 @@
+// Chapter No.1
+
+
+
+
+// Question 1
+
+// alert("Hello")
+
+// Queastion 2
+
+// alert("Error! Please enter a valid password")
+
+// Question 3
+
+// alert("Wellcome to JS Land... \n Happy Coding")
+
+// Question 4(a)
+
+// alert("Wellcome to JS Land...")
+
+// Question 4(b)
+
+// alert ("Happy Coding \n Prevent this page from creating additional dialogs")
+
+// Question 5
+
+// alert("Hello... I can run JS through my web browser's console")
+
+
+
+
+// Chapter No.2
+
+
+
+
+// Question 1
+
+// var username = "Ali"
+
+// alert(username)
+
+// Question 2
+
+// var name = "Muhammad Muddassir"
+
+// alert(name)
+
+// Question 3
+
+// var message = "Hello World"
+
+// alert(message)
+
+// Question 4
+
+// var name = "Jhone Doe"
+// var age = "15 years old"
+// var qualification = "Certified Mobile Application Development"
+
+// alert(name)
+// alert(age)
+// alert(qualification)
+
+// Question 5
+
+// var words = "PIZZA \nPIZZ \nPIZ \nPI \nP"
+
+// alert(words)
+
+// Question 6
+
+// var email = "My email address is example@example.com"
+
+// alert(email)
+
+// Question 7
+
+// var book = "I am trying to learn from the Book A smarter way to learn JavaScript"
+
+// alert(book)
+
+// Question 8
+
+// var text = "Yah! I can write HTML content through JavaScript"
+
+// document.write(text)
+
+// Question 9
+
+// var message = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+
+// alert(message)
+// document.write(message)
+
+
+
+
+// Chapter No.3
+
+
+
+
+// Question 1
+
+// var age = "I am 15 years old"
+
+// alert(age)
+
+// Question 2
+
+// var visitedTime = "You have visited this site 14 times"
+
+// alert(visitedTime)
+
+// Question 3
+
+// var birthYear = "My birth year is 1990 <br> Data type of my ddeclared variable is number"
+
+// document.write(birthYear)
+
+// Question 4
+
+// var text = "<b>John Doe</b> ordered <b>5 T-shirt</b>(s) on XYZ Clothing store"
+
+// document.write(text)
+
+// var visitorName = "John Doe"
+// var productTitle = "XYZ Clothing store"
+// var quantity = "T-shirt(s)"
+
+// alert(visitorName)
+// alert(productTitle)
+// alert(quantity)
+
+
+
+
+// Chapter No.4
+
+
+
+// Question 1
+
+// var myFullName = "Ali Ahmed"
+
+// alert(myFullName)
+
+// Question 2
+
+// {legal variable names}
+// var _name = "Ali"
+// var myName = "Ali Ahmed"
+// var $my_1stVariable = "Ali"
+// var myFullName = "Ali Ahmed"
+// var text1 = "good"
+
+// alert(_name)
+// alert(myName)
+// alert($my_1stVariable)
+// alert(myFullName)
+// alert(text1)
+
+// {illegal variable names}
+// var 1name = "Ali"
+// var my Name = "Ali Ahmed"
+// var 1 st-Variable = "Ali"
+// var .alert = "Ali Ahmed"
+// var var = "good"
+
+// alert(1name)
+// alert(my Name)
+// alert(1 st-Variable)
+// alert(.alert)
+// alert(var)
+
+
+// Question 3
+
+document.write("<b>Rules for naming JS variables</b> <br><br><br> Variable name can contain only letters, numbers, dollar signs, and underscores. For example $my_1stVariable <br> Variables must begin with a letter, dollar signs, and underscores. For example $name, _name or name <br> Variable names are case sensitive <br> Variable names should not be JS keywords")
+
+// {Print on Console}
+console.log("Ali Ahmed")
+
+
+
+
+
